5d74b0071f34e3c18595707f24c41201d8080dd6
unknown <gottfriedcpn@gmail.com>
2024-06-13 13:26:31 +0700

    url sibijaks admin

--- hp_awards_project/urls.py
+++ hp_awards_project/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin-sibijak/", admin.site.urls),
+    path("admin-sibijaks/", admin.site.urls),
     path("", include("hp_awards.urls")),
 ]
 
