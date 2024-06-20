2107ff40ef636cc2ba2e1268e1250b9a6d65553c
unknown <gottfriedcpn@gmail.com>
2024-06-20 15:45:50 +0700

    salah rumus

--- hp_awards/views_htmx.py
+++ hp_awards/views_htmx.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 
 from .models import Submisi
@@ -8,9 +9,18 @@ def get_stats(request):
     mhs = Submisi.MAHASISWA
     umum = Submisi.UMUM
     submisis = Submisi.objects.prefetch_related("kolaborators").all()
+    submisis = submisis.annotate(jumlah_kolaborator=Count("kolaborators"))
     context["jumlah_submisi"] = submisis.count()
-    context["kategori_mhs_indiv"] = submisis.filter(kategori_pendaftar=mhs, kolaborators__isnull=True).count()
-    context["kategori_mhs_tim"] = submisis.filter(kategori_pendaftar=mhs, kolaborators__isnull=False).count()
-    context["kategori_umum_indiv"] = submisis.filter(kategori_pendaftar=umum, kolaborators__isnull=True).count()
-    context["kategori_umum_tim"] = submisis.filter(kategori_pendaftar=umum, kolaborators__isnull=False).count()
+    context["kategori_mhs_indiv"] = submisis.filter(
+        kategori_pendaftar=mhs, jumlah_kolaborator=0
+    ).count()
+    context["kategori_mhs_tim"] = submisis.filter(
+        kategori_pendaftar=mhs, jumlah_kolaborator__gt=0
+    ).count()
+    context["kategori_umum_indiv"] = submisis.filter(
+        kategori_pendaftar=umum, jumlah_kolaborator=0
+    ).count()
+    context["kategori_umum_tim"] = submisis.filter(
+        kategori_pendaftar=umum, jumlah_kolaborator__gt=0
+    ).count()
     return render(request, "hp_awards/htmx/stats.html", context)
